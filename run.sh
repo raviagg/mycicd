4638d37f863296d8d801bf1e1aeba9a2b456f412
Ravi Aggarwal <meetraviagg@gmail.com>
2026-03-14 20:18:09 -0700

    Fix argo

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# Create argocd namespace
+kubectl create ns argocd
+echo "**** Namespace argocd created"
+
 # Create shared namespace
 kubectl create ns ns-mycicd
 echo "**** Namespace ns-mycicd created"
@@ -5,18 +9,18 @@ echo "**** Namespace ns-mycicd created"
 #########################
 # Argo Setup
 #########################
-kubectl apply -n ns-mycicd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "**** Deployed argo in K8, waiting for pods to be ready for 5secs"
 
 # kubectl wait --for=condition=ready service -l app.kubernetes.io/part-of=argocd --timeout=30s
-# kubectl wait --for=create secret/argocd-initial-admin-secret -n ns-mycicd --timeout=30s
+# kubectl wait --for=create secret/argocd-initial-admin-secret -n argocd --timeout=30s
 sleep 5
 echo "**** 5secs wait is completed"
 
-kubectl port-forward svc/argocd-server -n ns-mycicd 8090:443 &
+kubectl port-forward svc/argocd-server -n argocd 8090:443 &
 echo "**** Port forwarded argo service to 8090"
 
-kubectl -n ns-mycicd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo
 echo "**** Connect to argo on localhost:8090, with username=admin and password printed above"
 
 #########################
